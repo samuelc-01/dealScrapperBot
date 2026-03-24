3bed5a1a96a40c44fbfb2ecd36f23bd05919ffff
samuelc-01 <samuelc.01dev@gmail.com>
2026-03-24 14:05:59 +0100

    fire!!!!!!!

--- src/services/filter.ts
+++
@@ -1,5 +0,0 @@
-import { Deal } from '../domain/deal';
-
-export function filterDealsByPrice(deals: Deal[], maxPrice: number): Deal[] {
-  return deals.filter((deal) => deal.numericPrice > 0 && deal.numericPrice <= maxPrice);
-}
--- src/test.ts
+++
@@ -1,8 +0,0 @@
-import { runDealPipeline } from './services/pipeline';
-
-async function test() {
-  const summary = await runDealPipeline();
-  console.log('Pipeline summary:', summary);
-}
-
-test();
